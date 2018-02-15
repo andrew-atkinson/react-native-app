de64590daadbdecc8220ca310a3f14cb3cdc2a08
andrew-atkinson <blind.sam@gmail.com>
2018-02-15 16:56:10 -0500

    fixes: error in reducer (didn't add '...state' to returned state)

--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -30,6 +30,7 @@ const reducer = (state = initialState, action) => {
 
     case SELECT_PLACE:
       return {
+        ...state,
         selectedPlace: state.places.find(place => {
             return place.key === action.placeKey
           })
