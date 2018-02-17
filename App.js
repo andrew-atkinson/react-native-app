b82f5044246743ec1c8c2d38ea6a6cfbb98dbb7d
andrew-atkinson <blind.sam@gmail.com>
2018-02-17 17:44:45 -0500

    adds: redux store to react-native-navigation

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
 import {Navigation} from 'react-native-navigation'
+import {Provide} from 'redux'
 
 import AuthScreen from './src/screens/Auth'
 import FindPlaceScreen from './src/screens/FindPlace'
 import SharePlaceScreen from './src/screens/SharePlace'
+import configureStore from './src/store/config'
+
+const store = configureStore();
 
 //register screens
-Navigation.registerComponent("places.AuthScreen", () => AuthScreen)
-Navigation.registerComponent("places.FindPlaceScreen", () => FindPlaceScreen)
-Navigation.registerComponent("places.SharePlaceScreen", () => SharePlaceScreen)
-
+Navigation.registerComponent("places.AuthScreen", () => AuthScreen, store, Provider)
+Navigation.registerComponent("places.FindPlaceScreen", () => FindPlaceScreen, store, Provider)
+Navigation.registerComponent("places.SharePlaceScreen", () => SharePlaceScreen, store, Provider)
 
 //start an app
 Navigation.startSingleScreenApp({
-  screen:{
-    screen:"places.AuthScreen",
+  screen: {
+    screen: "places.AuthScreen",
     title: "Login"
   }
 })
