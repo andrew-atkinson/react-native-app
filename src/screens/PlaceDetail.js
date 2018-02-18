24ab62ebdc97b2beac3b9508bb3a4bb24f9520d2
andrew-atkinson <blind.sam@gmail.com>
2018-02-17 19:38:12 -0500

    move: PlaceDetail is now a 'screen' not a 'component'

---
+++ src/screens/PlaceDetail.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react'
+import {
+  View,
+  Text,
+  Button,
+  Image,
+  StyleSheet,
+  TouchableOpacity
+} from "react-native"
+import {connect} from 'react-redux'
+import {deletePlace} from '../store/actions'
+
+import Icon from 'react-native-vector-icons/Ionicons'
+
+class PlaceDetail extends Component {
+
+  deletePlaceHandler = () => {
+    this.props.onDeletedPlace(this.props.selectedPlace.key)
+    this.props.navigator.pop();
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image source={this.props.selectedPlace.image} style={styles.placeImage}/>
+        <Text style={styles.placeName}>{this.props.selectedPlace.name}</Text>
+        <TouchableOpacity onPress={this.deletePlaceHandler} style={styles.trash}>
+          <Icon name="ios-trash" color="red" size={30}/>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 22
+  },
+  placeImage: {
+    height: 200,
+    width: "100%"
+  },
+  placeName: {
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 30,
+    margin: 20
+  },
+  trash: {
+    alignItems: "center"
+  }
+})
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onDeletedPlace: key => dispatch(deletePlace(key))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(PlaceDetail)
