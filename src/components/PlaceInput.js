8b289f66062525fe8e6f906b2b87bb7ae6ea654e
andrew-atkinson <blind.sam@gmail.com>
2018-03-01 18:52:02 -0500

    refactor: placeInput now a func, not a class

--- src/components/PlaceInput.js
+++ src/components/PlaceInput.js
@@ -1,54 +1,12 @@
 import React, {Component} from 'react'
-import {View, Text, TextInput, StyleSheet, Button} from 'react-native'
+import {View, Text, TextInput, Button} from 'react-native'
+import DefaultInput from './UI/DefaultInput'
 
-export default class PlaceInput extends Component {
-  constructor(props) {
-    super(props)
-  }
-
-  state = {
-    placeName: ''
-  }
-
-  placeNameChangeHandler = val => {
-    this.setState({placeName: val});
-  }
-
-  placeNameSubmitHandler = () => {
-    if (this.state.placeName.trim() === '') {
-      return;
-    }
-    this.props.onPlaceAdded(this.state.placeName)
-  }
-
-  render() {
-    return (
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder={'A place to visit'}
-          style={{width: 300}}
-          onChangeText={this.placeNameChangeHandler}
-          style={styles.placeInput}/>
-        <Button
-          title='add'
-          style={styles.placeButton}
-          onPress={this.placeNameSubmitHandler}/>
-      </View>
-    )
-  }
+const PlaceInput = props => {
+  return (<DefaultInput
+    placeholder="Place Name"
+    value={props.placeName}
+    onChangeText={props.onChangeText}/>)
 }
 
-const styles = StyleSheet.create({
-  inputContainer: {
-    width: "100%",
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: "center"
-  },
-  placeInput: {
-    width: "70%"
-  },
-  placeButton: {
-    width: "30%"
-  }
-})
+export default PlaceInput
