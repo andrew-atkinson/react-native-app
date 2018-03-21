9e468f998ce9e30d184e7dea28067c1a19c2dd71
andrew-atkinson <blind.sam@gmail.com>
2018-03-21 17:56:22 -0400

    adds: fade out button, and fade in placeList

--- src/screens/FindPlace.js
+++ src/screens/FindPlace.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
-import {View} from 'react-native'
+import {View, TouchableOpacity, StyleSheet, Text, Animated} from 'react-native'
 import {connect} from 'react-redux'
 import PlaceList from '../components/PlaceList'
 
 class FindPlaceScreen extends Component {
+  static navigatorStyle = {
+    navBarButtonColor: "orange"
+  }
+
+  state = {
+    placesLoaded: false,
+    removeAnim: new Animated.Value(1),
+    fadeList: new Animated.Value(0)
+  }
+
   constructor(props) {
     super(props)
     this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent)
@@ -26,14 +36,87 @@ class FindPlaceScreen extends Component {
     })
   }
 
+  placesSearchHandler = () => {
+    Animated.timing(this.state.removeAnim, {
+      toValue: 0,
+      duration: 500,
+      useNativeDriver: true
+    }).start(() => {
+        this.setState({placesLoaded: true})
+        this.placesLoadedHandler()
+      }
+    )
+  }
+
+  placesLoadedHandler = () => {
+    Animated.timing(this.state.fadeList, {
+      toValue: 1,
+      duration: 500,
+      useNativeDriver: true
+    }).start()
+  }
+
   render() {
+    let content = (
+      <Animated.View 
+        style={{
+          opacity: this.state.removeAnim,
+          transform: [{
+            scale: this.state.removeAnim.interpolate({
+              inputRange: [0,1],
+              outputRange:[12, 1]
+            })
+          }]
+        }}
+      >
+        <TouchableOpacity onPress={this.placesSearchHandler}>
+          <View style={styles.searchButton}>
+            <Text style={styles.searchText}>Find Places</Text>
+          </View>
+        </TouchableOpacity>
+      </Animated.View>
+    )
+
+    if (this.state.placesLoaded) {
+      content = (
+        <Animated.View
+          style={{
+            opacity: this.state.fadeList
+          }}
+        >
+          <PlaceList 
+            places={this.props.places} 
+            onItemSelected={this.itemSelectedHandler}/
+          >
+        </Animated.View>
+      )
+    } 
     return (
-      <View>
-        <PlaceList places={this.props.places} onItemSelected={this.itemSelectedHandler}/>
+      <View style={this.state.placesLoaded ? null : styles.buttonContainer}>
+        {content}
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  buttonContainer:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  searchButton : {
+    borderColor : "orange",
+    borderWidth: 3,
+    borderRadius: 50,
+    padding: 20,
+  },
+  searchText: {
+    color: "orange",
+    fontWeight: "bold",
+    fontSize: 26
+  }
+})
 
 const MapStateToProps = state => {
   return {
