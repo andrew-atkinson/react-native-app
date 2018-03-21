b3663ef94237ad298c64a6a377b6bb76caa61cb1
andrew-atkinson <blind.sam@gmail.com>
2018-03-21 17:57:15 -0400

    adds: responsiveness for different rotations

--- src/screens/PlaceDetail.js
+++ src/screens/PlaceDetail.js
@@ -6,14 +6,36 @@ import {
   Image,
   StyleSheet,
   TouchableOpacity,
-  Platform
-} from "react-native"
+  Platform,
+  Dimensions
+} from 'react-native'
 import {connect} from 'react-redux'
 import {deletePlace} from '../store/actions'
 
 import Icon from 'react-native-vector-icons/Ionicons'
 
 class PlaceDetail extends Component {
+  state = {
+    viewMode: Dimensions.get('window').height > 500 ? 'portrait' : 'landscape'
+  }
+
+  constructor(props) {
+    super(props)
+    Dimensions.addEventListener('change', this.updateStyles)
+  }
+
+  componentWillUnmount() {
+    Dimensions.removeEventListener('change', this.updateStyles)
+  }
+
+  updateStyles = dims => {
+    this.setState({
+      viewMode: dims.window.height > 500
+        ? 'portrait'
+        : 'landscape'
+    })
+  }
+
   deletePlaceHandler = () => {
     this.props.onDeletedPlace(this.props.selectedPlace.key)
     this.props.navigator.pop()
@@ -21,33 +43,46 @@ class PlaceDetail extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Image source={this.props.selectedPlace.image} style={styles.placeImage}/>
-        <Text style={styles.placeName}>{this.props.selectedPlace.name}</Text>
-        <TouchableOpacity onPress={this.deletePlaceHandler} style={styles.trash}>
-          <Icon name={Platform.OS === 'android' ? "md-trash" : "ios-trash"} color="red" size={30}/>
-        </TouchableOpacity>
+      <View style={this.state.viewMode === 'landscape' ? styles.landscapeContainer : styles.portraitContainer}>
+        <Image source={this.props.selectedPlace.image} style={this.state.viewMode === 'landscape' ? styles.landscapePlaceImage : styles.portraitPlaceImage}/>
+        <View>
+          <Text style={styles.placeName}>{this.props.selectedPlace.name}</Text>
+          <TouchableOpacity onPress={this.deletePlaceHandler} style={styles.trash}>
+            <Icon name={Platform.OS === 'android' ? 'md-trash' : 'ios-trash'} color='red' size={30}/>
+          </TouchableOpacity>
+        </View>
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    margin: 22
+  portraitContainer: {
+    margin: 22,
+    flexDirection: 'column',
+    justifyContent: 'flex-start'
   },
-  placeImage: {
+  landscapeContainer: {
+    margin: 22,
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  portraitPlaceImage: {
     height: 200,
-    width: "100%"
+    width: '100%'
+  },
+  landscapePlaceImage: {
+    height: 200,
+    width: '60%'
   },
   placeName: {
-    textAlign: "center",
-    fontWeight: "bold",
+    textAlign: 'center',
+    fontWeight: 'bold',
     fontSize: 30,
     margin: 20
   },
   trash: {
-    alignItems: "center"
+    alignItems: 'center'
   }
 })
 
