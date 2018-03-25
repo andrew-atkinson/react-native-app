55c624f9fc6b2c18e01e81846cb571e69ea980e8
andrew-atkinson <blind.sam@gmail.com>
2018-03-24 22:53:08 -0400

    adds: airbnb react native maps

--- android/app/src/main/java/com/reactnativeapp/MainApplication.java
+++ android/app/src/main/java/com/reactnativeapp/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.reactnativenavigation.NavigationApplication;
+import com.airbnb.android.react.maps.MapsPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -58,7 +59,8 @@ public class MainApplication extends NavigationApplication {
          // Add additional packages you require here
          // No need to add RnnPackage and MainReactPackage
          return Arrays.<ReactPackage>asList(
-             new VectorIconsPackage()
+             new VectorIconsPackage(),
+             new MapsPackage()
          );
      }
 
