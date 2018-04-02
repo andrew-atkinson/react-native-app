64573fe0fec916830ff8624c82eea77ee4353d63
andrew-atkinson <blind.sam@gmail.com>
2018-04-02 18:57:40 -0400

    adds: thunk middleware for uploading to firebase and fb storage

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,17 +1,41 @@
 import {ADD_PLACE, DELETE_PLACE} from './actionTypes'
+import {uiStopLoading, uiStartLoading} from './index'
 
 export const addPlace = (placeName, location, image) => {
-  return {
-    type: ADD_PLACE, 
-    placeName,
-    location,
-    image
+  return dispatch => {
+    dispatch(uiStartLoading())
+    fetch('https://us-central1-react-native-app-1521939321832.cloudfunctions.net/storeImage', {
+      method: 'POST',
+      body: JSON.stringify({
+        image: image.base64
+      })
+    })
+    .catch(err => {
+      console.log('err', err)
+      dispatch(uiStopLoading())
+    })
+    .then(res => res.json())
+    .then(parsedRes => {
+      const placeData = {
+        name: placeName,
+        location,
+        image: parsedRes.imageUrl
+      }
+      return fetch('https://react-native-app-1521939321832.firebaseio.com/places.json', {
+        method: 'POST',
+        body: JSON.stringify(placeData)
+      })
+    })
+    .catch(err => {
+      console.log('err', err)
+      dispatch(uiStopLoading)
+    })
+    .then(res => res.json())
+    .then(parsedRes => {
+      console.log(parsedRes)
+      dispatch(uiStopLoading())
+    })
   }
 }
 
-export const deletePlace = key => {
-  return {
-    type: DELETE_PLACE,
-    placeKey: key
-  }
-}
+export const deletePlace = key => ({type: DELETE_PLACE, placeKey: key})
