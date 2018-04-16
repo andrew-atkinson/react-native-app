141f195b262bc3aec551ebdc63654c6754fa5cec
andrew-atkinson <blind.sam@gmail.com>
2018-04-16 14:34:30 -0400

    fixes: rebuilt firebase project - adds new URIs

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -5,7 +5,7 @@ export const addPlace = (placeName, location, image) => {
   console.log('placeName, location, image', placeName, location, image)
   return dispatch => {
     dispatch(uiStartLoading())
-    fetch('https://us-central1-react-native-app-1521939321832.cloudfunctions.net/storeImage', {
+    fetch('https://us-central1-react-native-places.cloudfunctions.net/storeImage', {
       method: 'POST',
       body: JSON.stringify({
         image: image.base64
@@ -23,14 +23,14 @@ export const addPlace = (placeName, location, image) => {
         location,
         image: parsedRes.imageUrl
       }
-      return fetch('https://react-native-app-1521939321832.firebaseio.com/places.json', {
+      return fetch('https://react-native-places.firebaseio.com/places.json', {
         method: 'POST',
         body: JSON.stringify(placeData)
       })
     })
     .catch(err => {
       console.log('err', err)
-      alert("location: Something went wrong, please try again!")      
+      alert("location: Something went wrong, please try again!")
       dispatch(uiStopLoading())
     })
     .then(res => res.json())
@@ -43,7 +43,7 @@ export const addPlace = (placeName, location, image) => {
 
 export const getPlaces = () => {
   return dispatch => {
-    fetch('https://react-native-app-1521939321832.firebaseio.com/places.json')
+    fetch('https://react-native-places.firebaseio.com/places.json')
     .catch(err => {
       alert("something went wrong...")
       console.log(err)
@@ -74,15 +74,15 @@ export const setPlaces = places => {
 
 export const deletePlace = key => {
   return dispatch => {
-    fetch('https://react-native-app-1521939321832.firebaseio.com/places/' + key + '.json', {method: 'DELETE'})
+    fetch('https://react-native-places.firebaseio.com/' + key + '.json', {method: 'DELETE'})
     .catch(err => {
       console.log('err', err)
-      alert("location: Something went wrong, please try again!")      
+      alert("deleting: Something went wrong, please try again!")      
       dispatch(uiStopLoading())
     })
     .then(res => res.json())
     .then(parsedRes => {
-      console.log("done...", parsedRes)
+      console.log("done...")
       dispatch(deletePlaceAction(key))
     })
 
