f89d5dae260d1e9a66665a1c8a52ac1dd3a405b4
andrew-atkinson <blind.sam@gmail.com>
2018-04-22 08:00:27 -0400

    adds: tokens to the add/delete/get places actions

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,91 +1,104 @@
 import {SET_PLACES, DELETE_PLACE} from './actionTypes'
-import {uiStopLoading, uiStartLoading} from './index'
+import {uiStopLoading, uiStartLoading, authGetToken} from './index'
 
 export const addPlace = (placeName, location, image) => {
-  console.log('placeName, location, image', placeName, location, image)
   return dispatch => {
-    dispatch(uiStartLoading())
-    fetch('https://us-central1-react-native-places.cloudfunctions.net/storeImage', {
-      method: 'POST',
-      body: JSON.stringify({
-        image: image.base64
-      })
+    let authToken;
+    dispatch(authGetToken())
+    .catch(err => alert('no valid token found:' + err))
+    .then(token => {
+      authToken = token
+      dispatch(uiStartLoading())
+      return fetch('https://us-central1-react-native-places.cloudfunctions.net/storeImage', 
+        {
+          method: 'POST',
+          body: JSON.stringify({image: image.base64}),
+          headers: {
+            "Authorization" : "Bearer " + authToken
+          }
+        }
+      )
     })
     .catch(err => {
+      alert('no valid token found:' + err)
       console.log('err', err)
       alert("Imagestore: Something went wrong, please try again!")
       dispatch(uiStopLoading())
     })
     .then(res => res.json())
     .then(parsedRes => {
+      console.log("parsedRes from imagestore:", parsedRes)
       const placeData = {
         name: placeName,
         location,
         image: parsedRes.imageUrl
       }
-      return fetch('https://react-native-places.firebaseio.com/places.json', {
+      return fetch('https://react-native-places.firebaseio.com/places.json?auth=' + authToken, {
         method: 'POST',
         body: JSON.stringify(placeData)
       })
-    })
-    .catch(err => {
-      console.log('err', err)
-      alert("location: Something went wrong, please try again!")
-      dispatch(uiStopLoading())
     })
     .then(res => res.json())
     .then(parsedRes => {
       console.log(parsedRes)
       dispatch(uiStopLoading())
     })
+    .catch(err => {
+      console.log('err', err)
+      alert("location: Something went wrong, please try again!")
+      dispatch(uiStopLoading())
+    })
   }
 }
-
+  
 export const getPlaces = () => {
-  return dispatch => {
-    fetch('https://react-native-places.firebaseio.com/places.json')
-    .catch(err => {
-      alert("something went wrong...")
-      console.log(err)
-    })
-    .then(res => res.json())
-    .then(parsedRes => {
-      let places = []
-      for (let key in parsedRes) {
-        places.push({
-          ...parsedRes[key],
-          image: {
-            uri: parsedRes[key].image
-          },
-          key
-        })
-      }
-      dispatch(setPlaces(places))
-    })
-  }
+  return dispatch => 
+  dispatch(authGetToken())
+  .then(token => fetch('https://react-native-places.firebaseio.com/places.json?auth=' + token))
+  .catch(err => alert('no valid token found:' + err))
+  .then(res => res.json())
+  .then(parsedRes => {
+    let places = []
+    for (let key in parsedRes) {
+      places.push({
+        ...parsedRes[key],
+        image: {
+          uri: parsedRes[key].image
+        },
+        key
+      })
+    }
+    dispatch(setPlaces(places))
+  })
+  .catch(err => {
+    alert("something went wrong...")
+    console.log(err)
+  })
 }
 
 export const setPlaces = places => {
-  return {
-    type: SET_PLACES,
-    places
-  }
+  return {type: SET_PLACES, places}
 }
 
 export const deletePlace = key => {
   return dispatch => {
-    fetch('https://react-native-places.firebaseio.com/' + key + '.json', {method: 'DELETE'})
-    .catch(err => {
-      console.log('err', err)
-      alert("deleting: Something went wrong, please try again!")      
-      dispatch(uiStopLoading())
+    dispatch(uiStartLoading())
+    dispatch(authGetToken())
+    .then(token => {
+      dispatch(deletePlaceAction(key))
+      return fetch('https://react-native-places.firebaseio.com/' + key + '.json?auth=' + token, {method: 'DELETE'})
     })
+    .catch(err => alert('no valid token found:' + err))
     .then(res => res.json())
     .then(parsedRes => {
       console.log("done...")
-      dispatch(deletePlaceAction(key))
+      dispatch(uiStopLoading())
     })
-
+    .catch(err => {
+      alert("something went wrong with deleting the entry")
+      console.log(err)
+      dispatch(uiStopLoading())
+    })
   }
 }
 
