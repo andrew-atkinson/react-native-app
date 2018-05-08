03126a1d26de0315aa71e1ba655f58f25d2eac68
andrew-atkinson <blind.sam@gmail.com>
2018-05-08 17:20:10 -0400

    adds: AsyncStorage for token

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -2,6 +2,7 @@ import {TRY_AUTH, AUTH_SET_TOKEN} from './actionTypes'
 import {APIKEY} from '../../../APIKEY.json'
 import {startMainTabs} from '../../screens/startMainTabs'
 import {uiStartLoading, uiStopLoading} from './index'
+
 import {AsyncStorage} from 'react-native'
 
 export const tryAuth = (authData, authMode) => dispatch => {
@@ -26,11 +27,10 @@ export const tryAuth = (authData, authMode) => dispatch => {
     return res.json()
   })
   .then(parsedRes => {
-    console.log('parsedRes', parsedRes)
     if (!parsedRes.idToken) {
       alert("Authentication error: " + parsedRes.error.message)
     } else {
-      dispatch(authStoreToken(parsedRes.idToken))
+      dispatch(authStoreToken(parsedRes.idToken, parsedRes.expiresIn))
       startMainTabs()
     }
   })
@@ -38,33 +38,52 @@ export const tryAuth = (authData, authMode) => dispatch => {
 
 export const authSetToken = token => ({type: AUTH_SET_TOKEN, token})
 
-export const authStoreToken = token => dispatch => {
+export const authStoreToken = (token, expiresIn) => dispatch => {
   dispatch(authSetToken(token))
-  AsyncStorage.setItem('places-token', token)
+  const now = new Date()
+  const expirationDate = now.getTime() + expiresIn * 1000
+  console.log("Now: ", now, new Date(expirationDate))
+  AsyncStorage.setItem('places:token', token)
+  AsyncStorage.setItem('expiration:date', expirationDate.toString())
 }
 
 export const authGetToken = () => (dispatch, getState) => {
   const promise = new Promise((resolve, reject) => {
-    const token = getState().auth.token
+    const token = getState().auth.token // checks for token stored in redux
     if (!token) {
-      AsyncStorage.getItem('places-token', () => reject())
+      let fetchedToken;
+      AsyncStorage.getItem('places:token') // if not, checks for token stored in global react native
+      .catch(() => reject())
       .then(storageToken => {
-        dispatch(authSetToken(storageToken))
-        resolve(storageToken)
+        fetchedToken = storageToken
+        return !storageToken ? reject() : AsyncStorage.getItem('expiration:date') // if there's a global react native token, call the expiration
+      })
+      .then(expirationDate => { // potentially, this COULD be null... 
+        const parsedExpirationDate = new Date(parseInt(expirationDate)) // parses a date from the expirationDate
+        const now = new Date()
+        if (parsedExpirationDate > now) { // checks if still valid (not valid if Null)
+          dispatch(authSetToken(fetchedToken)) // sets token on Redux
+          resolve(fetchedToken) // resolves with global react native token
+        } else {
+          reject()
+        }
       })
       .catch(err => reject())
     } else {
-      resolve(token)
+      resolve(token) // resolve with a redux token resolve, if it exists
     }
   })
-  return promise
+  .catch(err => { // if the promise has an error, remove the AsyncStorage. 
+    AsyncStorage.removeItem('expiration:date')
+    AsyncStorage.removeItem('places:token')
+  })
+  return promise //returns whatever is resolved/rejected
 }
-
 
 export const authAutoSignin = () => dispatch => {
   dispatch(authGetToken())
   .then(token => {
     startMainTabs()
   })
-  .catch(err => console.log('tokenless!'))
+  .catch(err => console.log('tokenless: failed to fetch token!'))
 }
