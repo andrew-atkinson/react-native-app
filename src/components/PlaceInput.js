9a79918ffef7b5cc7306093e4df2a6e8a7e34756
andrew-atkinson <blind.sam@gmail.com>
2018-05-30 20:43:39 -0400

    chore: minor tidying of the code

--- src/components/PlaceInput.js
+++ src/components/PlaceInput.js
@@ -2,12 +2,13 @@ import React, {Component} from 'react'
 import {View, Text, TextInput, Button} from 'react-native'
 import DefaultInput from './UI/DefaultInput'
 
-const PlaceInput = props => {
-  return (<DefaultInput
+const PlaceInput = props => (
+  <DefaultInput
     placeholder="Place Name"
     value={props.placeName}
     onChangeText={props.onChangeText}
-    {...props}/>)
-}
+    {...props}
+  />
+)
 
 export default PlaceInput
