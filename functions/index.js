b4c1633ac0d7f45a3244164fd865ef55960408ae
andrew-atkinson <blind.sam@gmail.com>
2018-06-03 20:57:02 -0400

    adds: delete image cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
-const functions = require('firebase-functions');
-const cors = require('cors')({origin: true})
+const functions = require('firebase-functions')
+const cors = require('cors')({ origin: true })
 const fs = require('fs')
 const firebaseKeys = require('./firebase-keys.json')
 
@@ -12,31 +12,29 @@ const gcconfig = {
 const gcs = require('@google-cloud/storage')(gcconfig)
 const UUID = require('uuid/v4')
 
-admin.initializeApp(admin.credential.cert(require('./private-key.json')));
+admin.initializeApp({
+  credential: admin.credential.cert(require('./private-key.json')),
+  databaseURL: 'https://react-native-places.firebaseio.com'
+})
 
 exports.storeImage = functions.https.onRequest((req, res) => {
-    cors(req, res, () => {
-      if (!req.headers.authorization || !req.headers.authorization.startsWith("Bearer ")) {
-        console.log("No token present!")
-        res.status(403).json({error: "unauthorized"})
-        return;
-      } 
-      
-      let idToken = req.headers.authorization.split("Bearer ")[1];
+  cors(req, res, () => {
+    if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) {
+      res.status(403).json({ error: 'unauthorized' })
+      return
+    }
 
-      admin.auth().verifyIdToken(idToken)
-      .then(decodedToken => {
+    const idToken = req.headers.authorization.split('Bearer ')[1]
+
+    admin.auth().verifyIdToken(idToken)
+      .then(() => {
         const body = JSON.parse(req.body)
-        console.log("body for image store", body)
-        fs.writeFileSync('/tmp/uploaded-image.jpg', body.image, 'base64', err => {
-          console.log("filesync error:", err)
-          return res.status(500).json({error: err})
-        })
+        fs.writeFileSync('/tmp/uploaded-image.jpg', body.image, 'base64', (err) => res.status(500).json({error: err}))
         const bucket = gcs.bucket('react-native-places.appspot.com')
         const uuid = UUID()
         return bucket.upload('/tmp/uploaded-image.jpg', {
           uploadType: 'media',
-          destination: '/places/' + uuid + '.jpg',
+          destination: `/places/${uuid}.jpg`,
           metadata: {
             contentType: 'image/jpeg',
             firebaseDownloadStorageTokens: uuid
@@ -44,17 +42,22 @@ exports.storeImage = functions.https.onRequest((req, res) => {
         }, (err, file) => {
           if (!err) {
             res.status(201).json({
-                imageUrl: "https://firebasestorage.googleapis.com/v0/b/" + bucket.name + '/o/' + encodeURIComponent(file.name) + '?alt=media&token=' + uuid
-              })
+              imageUrl: `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${encodeURIComponent(file.name)}?alt=media&token=${uuid}`,
+              imagePath: `/places/${uuid}.jpg`
+            })
           } else {
-            console.log('err', err)
-            res.status(500).json({error: err})
+            res.status(500).json({ error: err })
           }
         })
       })
-      .catch(err=> {
-        console.log('token invalid', err)
-        res.status(403).json({error: "Unauthorized"})
+      .catch((err) => {
+        res.status(403).json({ error: 'Unauthorized', err })
       })
-    })
-  });
+  })
+})
+
+exports.deleteImage = functions.database.ref('/places/{placeId}').onDelete(e => {
+  const imagePath = e.val().imagePath
+  const bucket = gcs.bucket('react-native-places.appspot.com')
+  return bucket.file(imagePath).delete()
+})
