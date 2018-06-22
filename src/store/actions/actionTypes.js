2c02565d0b876736f9d65e9d411a3934600a5ed8
andrew-atkinson <blind.sam@gmail.com>
2018-06-22 13:56:53 -0400

    removes: unneeded action type

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -3,8 +3,6 @@ export const DELETE_PLACE = 'DELETE_PLACE'
 export const PLACE_ADDED = 'PLACE_ADDED'
 export const START_ADD_PLACE = 'START_ADD_PLACE'
 
-export const TRY_AUTH = 'TRY_AUTH'
-
 export const UI_START_LOADING = 'UI_START_LOADING'
 export const UI_STOP_LOADING = 'UI_STOP_LOADING'
 
